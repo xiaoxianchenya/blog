cf9d548e7656ac6fb54a2a95f5d177aa93bd5fa9
44189 <v1613@vip.qq.com>
2023-12-20 14:46:36 +0800

    搜索功能

---
+++ src/test/java/com/traning/blog/DBTest.java
@@ -0,0 +1,30 @@
+package com.traning.blog;
+
+import com.traning.blog.mapper.BlogMapper;
+import com.traning.blog.pojo.Blog;
+import com.traning.blog.service.BlogService;
+import com.traning.blog.service.impl.BlogServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Slf4j
+public class DBTest {
+    @Autowired
+    private  BlogService blogService;
+    @Test
+    public void test(){
+        List<Blog> blogs = blogService.searchByTitle("maven");
+        for (Blog blog : blogs) {
+            log.error(blog.toString());
+        }
+    }
+
+}
